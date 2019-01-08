b482e090c7a4e3e780346ff0d40992370ebb19fa
liangjianfeng <769970587@qq.com>
2019-01-08 22:21:49 +0800

    l1 - l2

---
+++ lesson1/app.js
@@ -0,0 +1,11 @@
+let express = require('express')
+
+let app  = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world!')
+})
+
+app.listen(3000, function() {
+    console.log('app is listening at port 3000');
+})
